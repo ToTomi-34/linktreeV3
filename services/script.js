18fbe219bdfde18058aaa165ad274da8646a5e9e
Emanoel Dev <atomicraftx@gmail.com>
2026-05-13 01:51:14 -0300

    Add files via upload

---
+++ services/script.js
@@ -0,0 +1,7 @@
+function abrirSidebar() {
+    document.querySelector(".sidebar").classList.add("open");
+}
+
+function fecharSidebar() {
+    document.querySelector(".sidebar").classList.remove("open");
+}
