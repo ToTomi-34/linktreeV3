b1a0721b7a4f5c1d5651c35dbf8c6275d67a870c
Emanoel Dev <atomicraftx@gmail.com>
2026-05-13 01:49:42 -0300

    Delete Linktree/pages directory

--- Linktree/pages/contato/script.js
+++
@@ -1,7 +0,0 @@
-function abrirSidebar() {
-    document.querySelector(".sidebar").classList.add("open");
-}
-
-function fecharSidebar() {
-    document.querySelector(".sidebar").classList.remove("open");
-}
